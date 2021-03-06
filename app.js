cb219560bbcb8c644e7abcb78b2f0042693edcc3
Zdravko Sosa <zdravko.sosa@gmail.com>
2021-03-06 01:22:11 +0100

    Added sessions and flash messages

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 const ExpressError = require('./utils/ExpressError');
 
@@ -32,6 +34,25 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+  secret: 'secret',
+  resave: false, // for warnings to go away
+  saveUninitialized: true, // for warnings to go away
+  cookie: {
+    httpOnly: true, // security
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7, // week from now - ms * seconds * mins * hours * days
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
